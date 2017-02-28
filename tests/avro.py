ebebcf38b9e530dabe03742420c70c4e2438ac6c
Arnault <arnault@lal.in2p3.fr>
2017-02-28 14:07:18 +0100

    document the run command to run avro.py

--- tests/avro.py
+++ tests/avro.py
@@ -5,7 +5,7 @@ from pyspark import SparkConf, SparkContext
 import numpy as np
 
 """
-spark-submit --jars target/scala-2.11/spark-avro_2.11-3.2.1-SNAPSHOT.jar a.py
+spark-submit --jars $HOME/spark-avro/target/scala-2.11/spark-avro_2.11-3.2.1-SNAPSHOT.jar --executor-memory 12g --conf "spark.kryoserializer.buffer.max=2000mb" avro.py
 
 Documentation Python API
 http://spark.apache.org/docs/latest/api/python/pyspark.sql.html
