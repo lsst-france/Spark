2f38fd6cc8f5eddebdae030da974d10448859d72
Arnault <arnault@lal.in2p3.fr>
2017-10-27 06:59:30 +0000

    tuning Colore for complete file read

--- Colore/app.py
+++ Colore/app.py
@@ -1,6 +1,6 @@
 
 from pyspark.sql import SparkSession
-from pyspark.sql import functions
+from pyspark.sql import functions as F
 
 import os
 from astropy.io import fits
@@ -21,7 +21,7 @@ if os.path.exists('/mongo'):
     tmp = '/mongo/log/tmp/'
 elif os.path.exists('/home/ubuntu/'):
     where = '/home/ubuntu/'
-    cores = 8
+    cores = 6
     tmp = '/home/ubuntu/'
 else:
     print('where can I get fits files?')
@@ -34,15 +34,15 @@ print("cores = ", cores)
 spark = SparkSession\
        .builder\
        .appName("Colore")\
-       .config("spark.cores.max", "{}".format(cores))\
-       .config("spark.executor.memory=200g") \
        .config("spark.local.dir={}".format(tmp))\
        .config("spark.storage.memoryFraction=0")\
        .getOrCreate()
 
-       # .config("spark.local.dir=/mongo/log/tmp/")\
-       # .config("spark.storage.memoryFraction=0")\
 
+"""
+       .config("spark.cores.max", "{}".format(cores))\
+       .config("spark.executor.memory=20g") \
+"""
 
 # df.write.mode("overwrite").save("./images")
 
@@ -72,44 +72,52 @@ for row in range(10):
 
 data = hdu[1].data
 
-dfs = []
-
 s1 = stp.Stepper()
 s = stp.Stepper()
 
-subset = 1 
-steps = 100
-part = subset / steps
-block = int((data.size / subset) / steps)
+def build(sc, data, subset, steps):
+  dfs = []
 
-print("steps = ", steps, " part = ", part, " block = ", block, " total data = ", (block * steps))
+  subset = int(1.0 / subset)
 
-for i in range(steps):
-  start = int(i * block)
-  ra = data['RA'][start:start+block]
-  dec = data['DEC'][start:start+block]
-  z = data['Z_COSMO'][start:start+block]
-  dz = data['DZ_RSD'][start:start+block]
+  if subset <= 0:
+      return None
 
-  points = np.column_stack((ra, dec, z, dz))
+  part = subset / steps
+  block = int((data.size / subset) / steps)
 
-  dfs.append(sc.parallelize(points, 10).map(lambda x: (float(x[0]), float(x[1]), float(x[2]), float(x[3]))))
+  print("steps = ", steps, " part = ", part, " block = ", block, " total data = ", (block * steps))
 
-  # print("i=", i, " points=", points_df.take(10))
-  s.show_step("==> i={}".format(i))
 
-allp = sc.union(dfs).toDF(['RA', 'DEC', 'Z', 'DZ'])
+  for i in range(steps):
+    start = int(i * block)
+    ra = data['RA'][start:start+block]
+    dec = data['DEC'][start:start+block]
+    z = data['Z_COSMO'][start:start+block]
+    dz = data['DZ_RSD'][start:start+block]
+
+    points = np.column_stack((ra, dec, z, dz))
+
+    dfs.append(sc.parallelize(points, 10000).map(lambda x: (float(x[0]), float(x[1]), float(x[2]), float(x[3]))))
+
+    # print("i=", i, " points=", points_df.take(10))
+    s.show_step("==> i={}".format(i))
+
+  return sc.union(dfs).toDF(['RA', 'DEC', 'Z', 'DZ'])
+
+allp = build(sc, data, subset=1, steps=20)
 
 s1.show_step("==> total")
 
-print(allp.show(10))
+## print(allp.show(10))
 
-maxra = allp.agg({'RA' : "max"}).collect()[0]
-minra = allp.agg({'RA' : "min"}).collect()[0]
-maxdec = allp.agg({'DEC' : "max"}).collect()[0]
-mindec = allp.agg({'DEC' : "min"}).collect()[0]
+s2 = stp.Stepper()
 
+minmax = allp.agg(F.min('RA'), F.max('RA'), F.min('DEC'), F.max('DEC')).collect()
 
-print(maxra, minra, maxdec, mindec)
+s2.show_step("get min-max")
+
+print(minmax)
+
 
 
