eaa2fdcef4c178c2318b448207d98791941bd9cf
Arnault <arnault@lal.in2p3.fr>
2018-01-08 12:05:53 +0100

    add arguments to

--- Colore/FitsToHDFS.py
+++ Colore/FitsToHDFS.py
@@ -2,6 +2,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
+from pyspark import StorageLevel
+
 import os
 from astropy.io import fits
 import numpy as np
@@ -35,6 +37,9 @@ parser.add_argument('-partitions', type=int, default='1000', help='partitions')
 parser.add_argument('-coalesce', type=int, default='1000', help='coalesce')
 parser.add_argument('-steps', type=int, default='20', help='steps')
 parser.add_argument('-message', default='200m', help='message')
+parser.add_argument('-output', default='colore', help='output data area')
+parser.add_argument('-cores', type=int, default='{}'.format(cores), help='cores')
+
 
 args = parser.parse_args()
 
@@ -47,6 +52,8 @@ print("partitions = ", args.partitions)
 print("coalesce = ", args.coalesce)
 print("steps = ", args.steps)
 print("message = ", args.message)
+print("output = ", args.output)
+print("cores = ", args.cores)
 
 # exit()
 
@@ -59,7 +66,8 @@ spark = SparkSession\
        .config("spark.local.dir={}".format(tmp))\
        .config("spark.rpc.message.maxSize:{}".format(args.message)) \
        .config("spark.executor.memory={}".format(args.executor))\
-       .config("spark.driver.memory={}".format(args.driver))\
+       .config("spark.driver.memory={}".format(args.driver)) \
+       .config("spark.cores.max", "{}".format(args.cores)) \
     .getOrCreate()
 
 
@@ -68,7 +76,6 @@ spark = SparkSession\
        .config("spark.memory.fraction=0.8")\
        .config("spark.storage.memoryFraction=0")\
 
-       .config("spark.cores.max", "{}".format(cores))\
        
 """
 
@@ -165,10 +172,20 @@ df = rdd.toDF(['RA', 'DEC', 'Z', 'DZ'])
 
 s1.show_step("==> dataframe")
 
+"""
+# size = sc._jvm.org.apache.spark.util.SizeEstimator.estimate(rdd)
+
+df.persist(StorageLevel.MEMORY_ONLY)
+size = df.count()
+df.unpersist()
+
+print("size = ", size)
+"""
+
 print(df.show(10))
 
 # save the result
-df.write.mode("overwrite").save("./colore")
+df.write.mode("overwrite").save("./{}".format(args.output))
 
 s1.show_step('write data')
 
